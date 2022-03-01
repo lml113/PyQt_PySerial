2abe0d65dc1cf64f4661056184bd2daeaa2d6bc1
lml113 <3285275237@qq.com>
2022-03-02 00:41:59 +0800

    main.c

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import Ui_PyQt_PySerial
+from tool import Tool
 
 class myMainWindow(QMainWindow,Ui_PyQt_PySerial.Ui_MainWindow):
     def __init__(self):
@@ -13,16 +14,18 @@ class myMainWindow(QMainWindow,Ui_PyQt_PySerial.Ui_MainWindow):
     def InitUI(self):
         self.setWindowIcon(QIcon('./image/uart.ico'))
         self.setWindowTitle('串口助手')
-        self.textBrowser.setFont(QFont('Arial',16))
+        # self.textBrowser.setFont(QFont('Arial',16))
         self.settings = QSettings("config.ini",QSettings.IniFormat)
         self.settings.setIniCodec('UTF-8')
 
+        self.uart_port = ''
         self.config_uart_baud = self.settings.value("SETUP/UART_BAUD",0,type=str)
         self.comboBox_2.setCurrentText(self.config_uart_baud)
 
         self.radioButton_1.setChecked(True)
         self.radioButton_3.setChecked(True)
         # 绑定信号与槽
+        self.comboBox_1.currentIndexChanged.connect(self.comboBoxChanged)
         self.comboBox_2.currentIndexChanged.connect(self.comboBoxChanged)
         self.comboBox_3.currentIndexChanged.connect(self.comboBoxChanged)
         self.comboBox_4.currentIndexChanged.connect(self.comboBoxChanged)
@@ -33,10 +36,15 @@ class myMainWindow(QMainWindow,Ui_PyQt_PySerial.Ui_MainWindow):
         self.action_stop.triggered.connect(self.action_x_cd)
         self.action_pause.triggered.connect(self.action_x_cd)
         self.action_clear.triggered.connect(self.action_x_cd)
+        # 实例化Tool
+        self.tool = Tool(self)
     def comboBoxChanged(self):
         comboBox_x = self.sender()
+        self.tool.uart.uart_close()
         #print(comboBox_x.currentText())
-        if comboBox_x==self.comboBox_2:
+        if comboBox_x==self.comboBox_1:
+            self.uart_port = self.comboBox_1.currentText()
+        elif comboBox_x==self.comboBox_2:
             uart_baud = self.comboBox_2.currentText()
             print('current uaet baud is ',uart_baud)
             self.settings.setValue('SETUP/UART_BAUD',uart_baud)
@@ -49,18 +57,27 @@ class myMainWindow(QMainWindow,Ui_PyQt_PySerial.Ui_MainWindow):
         else :
             print(self.comboBox_6.currentText())
     def PushButton_send(self):
-        print(self.textEdit.toPlainText())
-        self.textBrowser.append(self.textEdit.toPlainText())
+        print('you clicked btn_send')
+        if self.tool.uart.err == 0:
+            send_data = self.textEdit.toPlainText()
+            self.tool.uart.send_uart_data(send_data)
+        else:
+            print('串未打开')
     def action_x_cd(self):
         action_x = self.sender()
         if action_x == self.action_start:
             print('start!!!')
+            if self.uart_port != '':
+                print(self.uart_port)
+                self.tool.uart.start_uart(self.uart_port,self.config_uart_baud)
         elif action_x == self.action_stop:
             print('stop!!!')
+            self.tool.uart.uart_close()
         elif action_x == self.action_pause:
             print('pause!!!')
         else :
             print('clear!!!')
+            self.textBrowser.clear()
 
 
 if __name__ == "__main__":
